9e21fa5141d5d37ff5afed0ad78504c845928f50
techSanjeev <techsanjeevsharma@gmail.com>
2018-06-01 14:39:49 +0530

    code clean up and added new element in first of list

--- app/src/main/java/net/simplifiedcoding/firebasedatabaseexample/MainActivity.java
+++ app/src/main/java/net/simplifiedcoding/firebasedatabaseexample/MainActivity.java
@@ -30,19 +30,11 @@ public class MainActivity extends AppCompatActivity {
     public static final String ARTIST_NAME = "net.simplifiedcoding.firebasedatabaseexample.artistname";
     public static final String ARTIST_ID = "net.simplifiedcoding.firebasedatabaseexample.artistid";
     public static final String ARTIST_LIST = "ARTIST_LIST";
-
-
-
-
     EditText editTextName;
     Spinner spinnerGenre;
     Button buttonAddArtist;
     ListView listViewArtists;
-
-    //a list to store all the artist from firebase database
     ArrayList<Artist> artists;
-
-    //our database reference object
     DatabaseReference databaseArtists;
 
     @Override
@@ -50,55 +42,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-
-        final RatingDialog ratingDialog = new RatingDialog.Builder(this)
-                .threshold(3)
-                .session(2)
-                .onRatingBarFormSumbit(new RatingDialog.Builder.RatingDialogFormListener() {
-                    @Override
-                    public void onFormSubmitted(String feedback) {
-
-                    }
-                }).build();
-
-        ratingDialog.show();
-        //getting the reference of artists node
         databaseArtists = FirebaseDatabase.getInstance().getReference("artists");
-
-
-        //getting views
-       /* editTextName = (EditText) findViewById(R.id.editTextName);
-        spinnerGenre = (Spinner) findViewById(R.id.spinnerGenres);*/
         listViewArtists = (ListView) findViewById(R.id.listViewArtists);
-
-     /*   buttonAddArtist = (Button) findViewById(R.id.buttonAddArtist);*/
-
-        //list to store artists
         artists = new ArrayList<Artist>();
-
-
-        //adding an onclicklistener to button
-       /* buttonAddArtist.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //calling the method addArtist()
-                //the method is defined below
-                //this method is actually performing the write operation
-                addArtist();
-            }
-        });*/
-
-        //attaching listener to listview
         listViewArtists.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                //getting the selected artist
                 Artist artist = artists.get(i);
-
-                //creating an intent
                 Intent intent = new Intent(getApplicationContext(), ArtistActivity.class);
-
-                //putting artist name and id to intent
                 intent.putExtra(ARTIST_ID, artist.getArtistId());
                 intent.putExtra(ARTIST_NAME, artist.getArtistName());
                 intent.putExtra("COUNT",i);
@@ -106,50 +57,30 @@ public class MainActivity extends AppCompatActivity {
                 bundle.putParcelableArrayList(ARTIST_LIST, artists);
                 intent.putExtras(bundle);
                 startActivity(intent);
-
-
-
-                //starting the activity with intent
-                startActivity(intent);
             }
         });
-
-        /*listViewArtists.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Artist artist = artists.get(i);
-                showUpdateDeleteDialog(artist.getArtistId(), artist.getArtistName());
-                return true;
-            }
-        });*/
-
-
     }
-
-
 
     @Override
     protected void onStart() {
         super.onStart();
-
-        //attaching value event listener
         databaseArtists.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
-                //clearing the previous artist list
                 artists.clear();
                 ProgressBar pgsBar = (ProgressBar)findViewById(R.id.pBar);
                 pgsBar.setVisibility(View.VISIBLE);
-                //iterating through all the nodes
+                ArrayList<Artist> newartists=new ArrayList<Artist>();
                 for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
                     //getting artist
                     Artist artist = postSnapshot.getValue(Artist.class);
                     //adding artist to the list
-                    artists.add(artist);
+                    newartists.add(artist);
                 }
                 pgsBar.setVisibility(View.GONE);
-                //creating adapter
+                newartists.addAll(artists);
+                artists=newartists;
+
                 ArtistList artistAdapter = new ArtistList(MainActivity.this, artists);
                 //attaching adapter to the listview
                 listViewArtists.setAdapter(artistAdapter);
@@ -157,42 +88,8 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
             }
         });
     }
 
-
-    /*
-    * This method is saving a new artist to the
-    * Firebase Realtime Database
-    * */
-    /*private void addArtist() {
-        //getting the values to save
-        String name = editTextName.getText().toString().trim();
-        String genre = spinnerGenre.getSelectedItem().toString();
-
-        //checking if the value is provided
-        if (!TextUtils.isEmpty(name)) {
-
-            //getting a unique id using push().getKey() method
-            //it will create a unique id and we will use it as the Primary Key for our Artist
-            String id = databaseArtists.push().getKey();
-
-            //creating an Artist Object
-            Artist artist = new Artist(id, name, genre);
-
-            //Saving the Artist
-            databaseArtists.child(id).setValue(artist);
-
-            //setting edittext to blank again
-            editTextName.setText("");
-
-            //displaying a success toast
-            Toast.makeText(this, "Artist added", Toast.LENGTH_LONG).show();
-        } else {
-            //if the value is not given displaying a toast
-            Toast.makeText(this, "Please enter a name", Toast.LENGTH_LONG).show();
-        }
-    }*/
 }
